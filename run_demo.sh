e1091dd87aeddf25741b82c16d1e1d3176d2e46b
Schlomo Schapiro <github@schlomo.schapiro.org>
2013-06-12 08:24:53 +0200

    Refactor to allow running different tests with a standardized setup.
    Add test to validate hostname in host certificate.

--- run_demo.sh
+++ run_demo.sh
@@ -20,7 +20,7 @@ ssh-keygen -t dsa -N "" -C "Host key for $myhost" -f keys/host_key
 tests_ok+=(create-host-key)
 
 # sign host key by CA
-ssh-keygen -s keys/ca -V +53w -I $myhost -h keys/host_key.pub
+ssh-keygen -s keys/ca -V +53w -I $myhost -h -n $myhost keys/host_key.pub
 tests_ok+=(sign-host-key)
 
 # seed work dir for installwatch
@@ -31,26 +31,46 @@ cp -rv keys/* work/TRANSL/etc/ssh/
 
 function cleanup {
 	set +x
+	echo ; echo
+	echo "SSH PKI Demo Test Results:" ; echo
 	test "$daemon" && kill $daemon
 	for t in "${tests_ok[@]}" ; do
 		echo "Succeeded $t"
 	done
-	[[ ${tests_ok[@]:-1} == *congratulations* ]] || echo "SOME TESTS FAILED!"
+	[[ ${tests_ok[@]:-1} == *congratulations* ]] || echo "SOME TESTS FAILED! Check work/ssh.log and work/sshd.log"
+}
+trap cleanup 0
+daemon=
+
+function start_daemon_run_ssh {
+	# $1 is label, remaining args are passed into eval and should return success
+	local label="$1" ; shift
+	local workdir=$(pwd)/work
+
+	# start ssh daemon
+	fakeroot installwatch -t -b -r $workdir /usr/sbin/sshd -ddd &>work/sshd.log &
+	daemon=$!
+
+	sleep 0.2
+	if ! kill -0 $daemon 2>/dev/null ; then
+		echo "FAILED TO START SSHD FOR $label"
+		exit 1
+	fi
+
+	# connect to our daemon and ask for date
+	if eval "fakeroot installwatch -t -r $workdir $@" 2>work/ssh.log ; then
+		tests_ok+=("$label")
+		sleep 0.2
+		if kill -0 $daemon 2>/dev/null ; then
+			echo "SSHD STILL RUNNING after $label"
+			exit 1
+		fi
+		daemon=
+	else
+		exit 1
+	fi
 }
 
-# start ssh daemon
-trap cleanup 0
-xterm -title "sshd - SSH PKI Demo" -e bash -c "fakeroot installwatch -t -b -r $(pwd)/work /usr/sbin/sshd -ddd 2>&1 | tee work/sshd.log"&
-daemon=$!
-
-sleep 1
-kill -0 $daemon && tests_ok+=(start-private-sshd-daemon)
-
-# connect to our daemon and ask for date
-if fakeroot installwatch -t -r $(pwd)/work sshpass -pdemo ssh -vvv -p 2222 root@$myhost date  +ssh-pki-demo_%c | grep ssh-pki-demo ; then
-	tests_ok+=(test-trusting-known-hosts-via-cert-and-login-with-password)
-	daemon=
-else
-	exit 1
-fi
+start_daemon_run_ssh test-trusting-known-hosts-via-cert-and-login-with-password sshpass -pdemo ssh -vvv root@$myhost  date  +ssh-pki-demo_%c \| grep ssh-pki-demo
+start_daemon_run_ssh test-that-hostname-in-cert-must-match sshpass -pdemo ssh -vvv root@localhost  date  +ssh-pki-demo_%c \; test '$?' -gt 0
 tests_ok+=("in running all tests, congratulations!")
